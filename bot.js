4163bb4f3ce2c1d2504327d2a77aeb12b62fee0d
aurcode <aurelio3927@gmail.com>
2020-08-28 12:16:28 -0400

    f

--- bot.js
+++ bot.js
@@ -20,4 +20,13 @@ bot.on('message', (msg) => {
   });
 });
 
+bot.on('message', (msg) => {
+
+  var Hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+  bot.sendMessage(msg.chat.id,"Hello dear user");
+  }
+
+  });
+
 module.exports = bot;
